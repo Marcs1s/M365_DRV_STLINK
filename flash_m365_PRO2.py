5a59059001da0b95510b22e5a3c04a1e3227cceb
Marcs1s <101540604+Marcs1s@users.noreply.github.com>
2022-03-14 06:43:47 +0000

    Update flash_m365_PRO2.py

--- flash_m365_PRO2.py
+++ flash_m365_PRO2.py
@@ -73,7 +73,7 @@ if __name__ == '__main__':
  sys.stdout.write('flashing...\n')
  sys.stdout.flush()
  oocd.write_binary(0x08000000,'boot.bin')
- oocd.write_binary(0x08001000,'DRV223.bin')
+ oocd.write_binary(0x08001000,'DRV247.bin')
  oocd.write_binary(0x0800f800,'data_temp.bin')
  sys.stdout.write('done\n')
 
